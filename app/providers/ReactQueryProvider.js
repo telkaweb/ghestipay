57ef9d81f84dc9ec440a20469963992ce008c5b9
saeedrb <saeed.rb71@gmail.com>
2026-06-21 15:06:59 +0330

    add config to react query for remove all cache

--- app/providers/ReactQueryProvider.js
+++ app/providers/ReactQueryProvider.js
@@ -10,15 +10,14 @@ export default function ReactQueryProvider({ children }) {
         defaultOptions: {
           queries: {
             retry: 1,
-            refetchOnWindowFocus: false,
+            refetchOnWindowFocus: true, // مهم
+            refetchOnMount: "always", // خیلی مهم
+            staleTime: 0,
+            gcTime: 0,
           },
         },
-      })
+      }),
   );
 
-  return (
-    <QueryClientProvider client={client}>
-      {children}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
